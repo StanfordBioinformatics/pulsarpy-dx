b60fc77d6dc5a653d31a5bb9080adcd5fd648a94
Nathan Watson <nathankw@DN0a22f376.SUNet>
2018-11-30 16:17:13 -0800

    Added S3 logging

--- pulsarpy_dx/scripts/import_seq_results.py
+++ pulsarpy_dx/scripts/import_seq_results.py
@@ -16,18 +16,24 @@ which sends out notification emails as specified in the configuration file {} in
 and unsuccessful circomstances.".format(conf_file). See more details at
 https://docs.google.com/document/d/1ykBa2D7kCihzIdixiOFJiSSLqhISSlqiKGMurpW5A6s/edit?usp=sharing
 and https://docs.google.com/a/stanford.edu/document/d/1AxEqCr4dWyEPBfp2r8SMtz8YE_tTTme730LsT_3URdY/edit?usp=sharing.
+
+If the --log-s3 flag is set, then the log files will be uploaded to S3 in the bucket specified by the
+environment variable PULSARPYDX_S3. The log files will be stored in this bucket by timestamp.
 """
 
 import argparse
+import datetime
 import logging
+import os
 
+import boto3
 import dxpy
 
 import pulsarpy.models
 import pulsarpy.utils
 from pulsarpy.elasticsearch_utils import MultipleHitsException
 import scgpm_seqresults_dnanexus.dnanexus_utils as du
-from pulsarpy_dx import logger
+from pulsarpy_dx import logger, LOG_DIR
 import pulsarpy_dx.utils as utils
 
 
@@ -40,13 +46,19 @@ def get_parser():
     parser.add_argument('-d',"--days-ago",type=int,default=30, help="""
         The number of days ago to query for new projects that are billed to {}.""".format(ENCODE_ORG)
     )
+    parser.add_argument("--log-s3", action="store_true", help="""
+        Presence of this option means to upload the log files to the S3 bucket indicated by the
+        environment variable PULSARPYDX_S3."""
+    )
     return parser
 
 def main():
     parser = get_parser()
     args = parser.parse_args()
+    log_s3 = args.log_s3
     days_ago = args.days_ago 
     days_ago = "-" + str(days_ago) + "d" 
+   
     projects = list(dxpy.find_projects(created_after=days_ago,billed_to=ENCODE_ORG))
     # projects is a list of dicts (was a generator)
     num_projects = len(projects)
@@ -56,6 +68,7 @@ def main():
             logger.debug("{}. {}".format(str(i + 1), projects[i]["id"]))
     else: 
         return
+
     for i in projects:
         proj_id = i["id"]
         try:
@@ -71,6 +84,19 @@ def main():
                 "to": pulsarpy.DEFAULT_TO,
             }
             res = pulsarpy.utils.send_mail(form=form, from_name="import_seq_results")
+        finally:
+            if log_s3:
+                s3 = boto3.resource('s3')
+                bucket = s3.Bucket(os.environ["PULSARPYDX_S3"])
+                # Add subfolder for the present day
+                upload_folder = str(datetime.date.today()) + "/"
+                bucket.put_object(Key=upload_folder)  # put_object() is idempotent
+                today_logs = os.path.join(LOG_DIR)
+                for logfile in os.listdir(today_logs):
+                    filepath = os.path.join(today_logs, logfile)
+                    key = os.path.join(upload_folder, logfile)
+                    bucket.upload_file(Key=key, Filename=filepath) 
+               
 
 
 def get_read_stats(barcode_stats, read_num):
